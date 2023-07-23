5158901399889e7d5dc412d752b841c24edbf9e3
Anjali Jha <113273069+ajlevi@users.noreply.github.com>
2023-07-23 20:17:17 +0530

    Add files via upload

---
+++ Wall.java
@@ -0,0 +1,24 @@
+package com.anjali;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Wall extends Actor{
+	private Image image;
+	
+	public Wall (int x,int y) {
+		super(x,y);
+		
+		initWall();
+	}
+
+	private void initWall() {
+		ImageIcon icon = new ImageIcon("src/resources/wall.png");
+		image = icon.getImage();
+		setImage(image);
+		
+	}
+
+}
+
